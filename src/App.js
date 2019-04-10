017ca6fc2f62dae0fac1dec6448cd99b3fa05b3d
ljhhhhhh <13216698987@163.com>
2019-04-10 16:16:32 +0800

    '1'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    );
+    return <div>{this.props.children}</div>;
   }
 }
 
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,60 +1,66 @@
 import React, { Component } from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Row, Col, Form, Input, Button, Checkbox } from "antd";
+import './index.scss'
 
 const FormItem = Form.Item;
 
 class Login extends Component {
   state = {};
+  handleSubmit = () => {
+    const data = this.props.form.getFieldsValue();
+    console.log(data, 'data');
+  }
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        <Form>
-          <FormItem label="用户名">
-            {getFieldDecorator("username", {
-              initialValue: "",
-              rules: [
-                {
-                  required: true,
-                  message: "用户名不能为空"
-                },
-                {
-                  min: 5,
-                  max: 10,
-                  message: "长度不在范围内"
-                },
-                {
-                  pattern: /^\w/g,
-                  message: "必须是字母或者数字"
-                }
-              ]
-            })(<Input placeholder="请输入用户名" />)}
-          </FormItem>
-          <FormItem label="密码">
-            {getFieldDecorator("password", {
-              initialValue: "",
-              rules: [
-                {
-                  required: true,
-                  message: "密码不能为空"
-                }
-              ]
-            })(<Input placeholder="请输入密码" />)}
-          </FormItem>
-          <FormItem label="记住密码">
-            {
-              getFieldDecorator('check', {
-                valuePropName: 'checked',
-                initialValue: true
-              })(
-                <Checkbox>记住密码</Checkbox>
-              )
-            }
-          </FormItem>
-          <FormItem>
-            <Button type="primary">登录</Button>
-          </FormItem>
-        </Form>
+      <div className="form-content">
+        <h2>登录</h2>
+        <Row type="flex" justify="center">
+          <Col xs={20} md={12} lg={6}>
+            <Form layout="vertical">
+              <FormItem label="用户名">
+                {getFieldDecorator("username", {
+                  initialValue: "",
+                  rules: [
+                    {
+                      required: true,
+                      message: "用户名不能为空"
+                    },
+                    {
+                      min: 5,
+                      max: 10,
+                      message: "长度不在范围内"
+                    },
+                    {
+                      pattern: /^\w/g,
+                      message: "必须是字母或者数字"
+                    }
+                  ]
+                })(<Input placeholder="请输入用户名" />)}
+              </FormItem>
+              <FormItem label="密码">
+                {getFieldDecorator("password", {
+                  initialValue: "",
+                  rules: [
+                    {
+                      required: true,
+                      message: "密码不能为空"
+                    }
+                  ]
+                })(<Input placeholder="请输入密码" />)}
+              </FormItem>
+              <FormItem label="记住密码">
+                {getFieldDecorator("check", {
+                  valuePropName: "checked",
+                  initialValue: true
+                })(<Checkbox>记住密码</Checkbox>)}
+              </FormItem>
+              <FormItem>
+                <Button type="primary" onClick={this.handleSubmit}>登录</Button>
+              </FormItem>
+            </Form>
+          </Col>
+        </Row>
       </div>
     );
   }
